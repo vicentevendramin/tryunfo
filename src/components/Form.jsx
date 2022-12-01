4f6a47a2ec15846faa569f62cfcef99f80761974
Vicente Vendramin <guesser.vicente@gmail.com>
2022-12-01 17:01:04 -0300

    Requisito 1: Crie o formulário que será usado para adicionar cartas ao baralho

---
+++ src/components/Form.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <form>
+        <label htmlFor="name">
+          Nome
+          <input
+            data-testid="name-input"
+            type="text"
+            name="name"
+            id="name"
+          />
+        </label>
+        <label htmlFor="description">
+          Descrição
+          <textarea
+            data-testid="description-input"
+            name="description"
+            id="description"
+          />
+        </label>
+        <label htmlFor="attr1">
+          Attr1
+          <input
+            data-testid="attr1-input"
+            type="number"
+            name="attr1"
+            id="attr1"
+          />
+        </label>
+        <label htmlFor="attr2">
+          Attr2
+          <input
+            data-testid="attr2-input"
+            type="number"
+            name="attr2"
+            id="attr2"
+          />
+        </label>
+        <label htmlFor="attr3">
+          Attr3
+          <input
+            data-testid="attr3-input"
+            type="number"
+            name="attr3"
+            id="attr3"
+          />
+        </label>
+        <label htmlFor="image">
+          Imagem
+          <input
+            data-testid="image-input"
+            type="text"
+            name="image"
+            id="image"
+          />
+        </label>
+        <label htmlFor="rare">
+          Raridade
+          <select
+            data-testid="rare-input"
+            name="rare"
+            id="rare"
+          >
+            <option value="normal">normal</option>
+            <option value="raro">raro</option>
+            <option value="muito raro">muito raro</option>
+          </select>
+        </label>
+        <label htmlFor="trunfo">
+          <input
+            data-testid="trunfo-input"
+            type="checkbox"
+            name="trunfo"
+            id="trunfo"
+          />
+          Super Trybe Trunfo
+        </label>
+        <button
+          data-testid="save-button"
+          type="submit"
+          id="submit"
+        >
+          Salvar
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
