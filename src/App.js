f6f9a3961af61f032574de18ffaf32da84f6dfc3
Vicente Vendramin <guesser.vicente@gmail.com>
2022-12-02 12:30:47 -0300

    Requisito 5: Faça a validação do botão de Salvar no formulário

--- src/App.js
+++ src/App.js
@@ -18,11 +18,32 @@ class App extends React.Component {
     isSaveButtonDisabled: true,
   };
 
+  validationFields = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+    } = this.state;
+
+    const max = 90;
+    const maxSum = 210;
+    const attr1 = Number(cardAttr1) >= 0 && Number(cardAttr1) <= max;
+    const attr2 = Number(cardAttr2) >= 0 && Number(cardAttr2) <= max;
+    const attr3 = Number(cardAttr3) >= 0 && Number(cardAttr3) <= max;
+    const sum = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= maxSum;
+    const empty = cardName !== '' && cardDescription !== '' && cardImage !== '';
+
+    this.setState({ isSaveButtonDisabled: !(attr1 && attr2 && attr3 && sum && empty) });
+  };
+
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
-    this.setState({ [name]: value });
+    this.setState({ [name]: value }, this.validationFields);
   };
 
   render() {
