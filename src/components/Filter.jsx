1dc0025dba902bb4675080aba2545f90f4cf4b4f
Vicente Vendramin <guesser.vicente@gmail.com>
2022-12-11 15:29:56 -0300

    Adiciona estilo ao Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -6,45 +6,52 @@ export default class Filter extends Component {
     const { onInputChange, rareFilter, trunfoFilter } = this.props;
 
     return (
-      <form>
+      <form className="filter-form">
         <h3>Filtro de buscas</h3>
-        <label htmlFor="nameFilter">
-          <input
-            data-testid="name-filter"
-            disabled={ trunfoFilter }
-            id="name-filter"
-            type="text"
-            name="nameFilter"
-            placeholder="Nome da Carta"
-            onChange={ onInputChange }
-          />
-        </label>
-        <label htmlFor="rareFilter">
-          <select
-            data-testid="rare-filter"
-            disabled={ trunfoFilter }
-            id="rare-filter"
-            name="rareFilter"
-            value={ rareFilter }
-            onChange={ onInputChange }
-          >
-            <option value="todas">todas</option>
-            <option value="normal">normal</option>
-            <option value="raro">raro</option>
-            <option value="muito raro">muito raro</option>
-          </select>
-        </label>
-        <label htmlFor="trunfoFilter">
-          <input
-            data-testid="trunfo-filter"
-            id="trunfoFilter"
-            type="checkbox"
-            name="trunfoFilter"
-            checked={ trunfoFilter }
-            onChange={ onInputChange }
-          />
-          Super Trunfo
-        </label>
+        <div className="filter-section">
+          <label className="filter-label" htmlFor="nameFilter">
+            Nome
+            <input
+              data-testid="name-filter"
+              className="input-type3"
+              disabled={ trunfoFilter }
+              id="name-filter"
+              type="text"
+              name="nameFilter"
+              placeholder="Nome da Carta"
+              onChange={ onInputChange }
+            />
+          </label>
+          <label className="filter-label" htmlFor="rareFilter">
+            Raridade
+            <select
+              data-testid="rare-filter"
+              className="input-type3"
+              disabled={ trunfoFilter }
+              id="rare-filter"
+              name="rareFilter"
+              value={ rareFilter }
+              onChange={ onInputChange }
+            >
+              <option value="todas">todas</option>
+              <option value="normal">normal</option>
+              <option value="raro">raro</option>
+              <option value="muito raro">muito raro</option>
+            </select>
+          </label>
+          <label className="filter-label" htmlFor="trunfoFilter">
+            <input
+              data-testid="trunfo-filter"
+              className="input-checkbox"
+              id="trunfoFilter"
+              type="checkbox"
+              name="trunfoFilter"
+              checked={ trunfoFilter }
+              onChange={ onInputChange }
+            />
+            Super Trunfo
+          </label>
+        </div>
       </form>
     );
   }
